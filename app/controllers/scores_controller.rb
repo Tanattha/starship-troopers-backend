effdf5440889318cb0e77cb7f4c3f503a1e74f28
Tanattha <tanattha.thuenim@gmail.com>
2020-08-07 11:33:16 -0400

    updated

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,12 +4,6 @@ class ScoresController < ApplicationController
       render json: ScoreSerializer.new(scores)
     end
 
-    def create
-      score = Score.create(username: params[:username])
-      render json: UserSerializer.new(user)
-     
-    end
-  
     def topten
       users_and_scores = Score.topten_users_and_scores
       render json: users_and_scores
